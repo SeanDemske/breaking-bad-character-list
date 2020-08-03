c432ec5447dd69aca6a8365562b97d9ccc69cbe3
Sean Demske <seandemske@gmail.com>
2020-08-03 11:25:57 -0400

    Updates

---
+++ precache-manifest.9ac030d9b1c4705f9b58e651aebddcf3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a5538cd78c04bf11ce216d1dc44dd9",
+    "url": "/breaking-bad-character-list/index.html"
+  },
+  {
+    "revision": "fdf8b33031e6730e7990",
+    "url": "/breaking-bad-character-list/static/css/main.baa02f0d.chunk.css"
+  },
+  {
+    "revision": "33f97e6b2b60781c3e5e",
+    "url": "/breaking-bad-character-list/static/js/2.a2730ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-character-list/static/js/2.a2730ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf8b33031e6730e7990",
+    "url": "/breaking-bad-character-list/static/js/main.938b024f.chunk.js"
+  },
+  {
+    "revision": "2e129d0938eeac6b52df",
+    "url": "/breaking-bad-character-list/static/js/runtime-main.dca38a0e.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-character-list/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-character-list/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-character-list/static/media/spinner.235b9b3e.gif"
+  }
+]);
